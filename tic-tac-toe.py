6bbe6e341498e7e065670e604f9f47c3e6a6655c
Younho Lee <nasopida1@gmail.com>
2017-11-26 14:19:53 +0900

    add win message

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,28 @@
 from tkinter import *
 
+def win():
+      #only one row
+      if list[0]["text"]==list[1]["text"]==list[2]["text"]!="     ":
+            return 1
+      if list[3]["text"]==list[4]["text"]==list[5]["text"]!="     ":
+            return 1
+      if list[6]["text"]==list[7]["text"]==list[8]["text"]!="     ":
+            return 1
+      #only one column
+      if list[0]["text"]==list[3]["text"]==list[6]["text"]!="     ":
+            return 1
+      if list[1]["text"]==list[4]["text"]==list[7]["text"]!="     ":
+            return 1
+      if list[2]["text"]==list[5]["text"]==list[8]["text"]!="     ":
+            return 1
+      #diagonal
+      if list[0]["text"]==list[4]["text"]==list[8]["text"]!="     ":
+            return 1
+      if list[2]["text"]==list[4]["text"]==list[6]["text"]!="     ":
+            return 1
+      else:
+            return 0
+
 def checked(i) :
       global player
       button = list[i]
@@ -10,12 +33,19 @@ def checked(i) :
       button["bg"] = "yellow"
 
       if player == "X" :
+            if win()==1:
+                  print("The winner is X")
+                  sys.exit(1)
             player = "O"
             button["bg"] = "yellow"
       else :
+            if win()==1:
+                  print("The winner is O")
+                  sys.exit(1)
             player = "X"
             button["bg"] = "lightgreen"
-
+      
+      
 window = Tk()
 player = "X"
 list= []
